a0b6f6f59182e4df11fc2e1fb088218d2285554b
unknown <tas23alex@mail.ru>
2014-09-04 16:47:03 +0400

    Добавлено изменение цвета текста у кнопки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/* -----------Options-------------- */
+
+//Slider border-radius
+
 $(function() {
     $( "#slider-border-radius" ).slider(
         {
@@ -24,7 +28,7 @@ $(function() {
 
    
 
-/* ------------------------------------- */
+//Slider border-size
 
 $(function() {
     $( "#slider-border-size" ).slider(
@@ -46,51 +50,60 @@ $(function() {
         var val = $('#slider-border-size').slider('value');
             style.borderWidth = val + 'px';
     }
+});
 
-    
-  });
+/* ------------------------------------- */
 
 (function (){
+
+  //Переменные
+
       var create = $ ('.result-button'),
       bgColorInput = $ ('#bg-color'),
       brColorInput = $ ('#border-color'),
-      btnText = $ ('#btn-text');
-     
-
+      btnText = $('.btn'),
+      btnNewText = $('#button'),
+      btnColorText =$ ('#text-color');
+      
+      //backgound-color
 
       bgColorInput.on('change' , function (){
           var newColor = '#' + $(this).val();
           create.css({
             'background-color':  newColor 
           });
-
-          
       });
+
+      //border-color
 
       brColorInput.on('change' , function (){
           var newColor = '#' + $(this).val();
           create.css({
             'border-color':  newColor 
           });
-
-          
       });
 
-      
+      //button-text
 
-      
-          
+      btnText.on('keyup' , function(){
+         btnNewText.text($(this).val());
+      });
 
+      //button-text-color
+
+      btnColorText.on('change' , function (){
+          var newColorText = '#' + $(this).val();
+          create.css({
+            'color':  newColorText 
+          });
+      });
 
 }());
 
-
-
-
-   $(document).ready ( function(){
+//$(document).ready ( function(){
  
-$(".btn").keyup(function() {
-         $('#button').text($(".btn").val());
-});
+//$(".btn").keyup(function() {
+        // $('#button').text($(".btn").val());
+//});
  
-});
+//});
