86d8967429b9126a7522f0bad598dd6d66b91af4
unknown <tas23alex@mail.ru>
2014-09-06 00:37:03 +0400

    Модульный JS

--- js/main.js
+++ js/main.js
@@ -1,140 +1,119 @@
-/* -----------Options-------------- */
+(function() {
 
-//Slider border-radius
-
-
-
-   
-
-//Slider border-size
-
-
-
-/* ------------------------------------- */
-
-(function (){
-
-  //Переменные
-
-      var create = $ ('.result-button'),
-      bgColorInput = $ ('#bg-color'),
-      brColorInput = $ ('#border-color'),
-      btnText = $('.btn'),
-      btnNewText = $('#button'),
-      btnColorText = $ ('#text-color'),
-      btnFont = $('#font'),
-      cssCodeResultArea = $('#resultcss'),
-      htmlCodeResultArea = $('#resulthtml');
-      
- $(function() {
-    $( "#slider-border-radius" ).slider(
-        {
-            min: 0,
-            max: 20,
-            range: 'min',
-            change: setInputsFromSlider,
-            slide: setInputsFromSlider,
-            stop: setInputsFromSlider
+    var app = {
+        initialize: function() {
+            this.setUpListeners();
+            this.updateResult(); 
+            this.setUpPlugins();
+            this.setUpPluginsTwo();
+            this.htmlArea();
         },
-        'value'
-      );
-
-        var btnBorderRadius = document.getElementsByClassName('result-button');
-        style = btnBorderRadius[0].style;
-
-    function setInputsFromSlider (event, ui) {
-        var val = $('#slider-border-radius').slider('value');
-            style.borderRadius = val + 'px';
-            
-         updateResult();  
-    }
-
-
-  });
-
-$(function() {
-    $( "#slider-border-size" ).slider(
-        {
-            min: 0,
-            max: 6,
-            range: 'min',
-            change: setInputsFromSlider,
-            slide: setInputsFromSlider,
-            stop: setInputsFromSlider
+        setUpListeners: function () {
+          //Slider(Ползунки)
+          $ ('#slider-border-radius').on('slidechange',$.proxy(this.borderRadChange, this));
+          $ ('#slider-border-size').on('slidechange',$.proxy(this.borderSizeChange, this));
+          //Color
+          $ ('#bg-color').on('change',$.proxy(this.BgColor, this));
+          $ ('#border-color').on('change',$.proxy(this.BrColor, this));
+          //Text
+          $ ('#btn-text').on('keyup',$.proxy(this.BtnText, this));
+          $ ('#text-color').on('change',$.proxy(this.BtnColorText, this));
+          $ ('#font').on('change',$.proxy(this.BtnFontText, this)); 
         },
-        'value'
-      );
 
-    var btnBorderWidth = document.getElementsByClassName('result-button');
-        style = btnBorderWidth[0].style;
+        //Кнопка
+        create : $ ('.result-button'),
 
-    function setInputsFromSlider (event, ui) {
-        var val = $('#slider-border-size').slider('value');
-            style.borderWidth = val + 'px';
-           updateResult();
-    }
-    
-});
-      //изменение backgound-color у кнопки
+        //Slider UI border-radius
+        setUpPlugins:function() {
+            $( "#slider-border-radius" ).slider(
+              {
+              range: 'min',
+              max: 20,
+              min: 0,
+              });
+        },
 
-      bgColorInput.on('change' , function (){
-          var newColor = '#' + $(this).val();
-          create.css({
-            'background-color':  newColor 
-          });
-      updateResult();
-      });
+        //Изменение border-radius
+        borderRadChange:function (e,ui) {
+            this.create.css({
+                'border-radius' : ui.value
+            });
+            this.updateResult();
+        },
 
-      //изменение border-color у кнопки
+        //Slider UI border-size
+        setUpPluginsTwo:function() {
+            $( "#slider-border-size" ).slider(
+              {
+              range: 'min',
+              max: 6,
+              min: 0,
+              });
+        },
 
-      brColorInput.on('change' , function (){
-          var newColor = '#' + $(this).val();
-          create.css({
-            'border-color':  newColor 
-          });
-          updateResult();
-      });
+        //Изменение border-size
+        borderSizeChange:function (e,ui) {
+            this.create.css({
+                'border-width' : ui.value
+            });
+            this.updateResult();
+        },
 
-      //button-text
+        //Изменение backgrouns-color
+        BgColor: function() {
+            var newColor = '#' + $('#bg-color').val();
+            this.create.css({
+                'background-color':  newColor 
+            });
+            this.updateResult();     
+        },
 
-      btnText.on('keyup' , function(){
-         btnNewText.text($(this).val());
-         htmlArea();
-      });
+        //Изменение border-color
+        BrColor: function() {
+            var newColor = '#' + $('#border-color').val();
+            this.create.css({
+                'border-color':  newColor 
+            });
+            this.updateResult();
+        },
 
-      //изменение text-color у текста кнопки
+        //Изменение текста кнопки
+        BtnText: function() {
+            $('#button').text($('#btn-text').val());
+            this.htmlArea();
+        },
 
-      btnColorText.on('change' , function (){
-          var newColorText = '#' + $(this).val();
-          create.css({
-            'color':  newColorText 
-          });
-          updateResult();
-      });
+        //Изменение цвета текста кнопки
+        BtnColorText: function() {
+            var newColorText = '#' + $('#text-color').val();
+            this.create.css({
+                'color':  newColorText 
+            });
+            this.updateResult();
+        },
 
-      //измение font-family у кнопки
-      btnFont.on('change' , function (){
-          var newFont = $(this).val();
-          create.css({
-            'font-family':  newFont
-          });
-          updateResult();
-      });
+        //Изменение шрифта кнопки
+        BtnFontText: function () {
+            var newFont = $('#font').val();
+            this.create.css({
+                'font-family':  newFont
+            });
+            this.updateResult(); 
+        },
 
-     var updateResult = function() {
+        //Динамическое изменение css textarea 
+        updateResult: function() {
+            var borderRad = this.create.css('border-radius'),
+                borderSize = this.create.css('border-width'),
+                fontFamily = this.create.css('font-family'),
+                bgcolor = this.create.css('background-color'),
+                brcolor = this.create.css('border-color'),
+                textcolor = this.create.css('color'),
+                cssCodeResultArea = $('#resultcss');
 
-      var borderRad = create.css('border-radius'),
-          borderSize = create.css('border-width'),
-          fontFamily = create.css('font-family'),
-          bgcolor = create.css('background-color'),
-          brcolor = create.css('border-color'),
-          textcolor = create.css('color');
-
-
-
-      cssCodeResultArea.text(
-
-          '.button {\n' +
+          cssCodeResultArea.text(
+            '.button {\n' +
             'font-family:'           + fontFamily + ';\n' +
             'background-color:'      + bgcolor + ';\n' +
             'border-color:'          + brcolor + ';\n' +
@@ -144,29 +123,24 @@ $(function() {
             'border-radius:'         + borderRad + ';\n' +
             'border-width:'          + borderSize + ';\n' +
             '};'
-        );
+          );
 
-     }
-      
-    updateResult();
+        },
 
-    var htmlArea = function(){
+        
+        //Динамическое изменение html textarea 
+        htmlArea: function(){
+            var htmlCodeResultArea = $('#resulthtml'),
+                text = $('.btn').val();
 
-      var text = $('.btn').val();
+            htmlCodeResultArea.text(
+                '<button class="create-button"> ' + text + ' </button>\n'
+            );
+        }
 
-      htmlCodeResultArea.text(
-          '<button class="create-button"> ' + text + ' </button>\n'
-        );
+
     }
 
-    htmlArea();
+    app.initialize();
+})();
 
-}());
-
-//$(document).ready ( function(){
- 
-//$(".btn").keyup(function() {
-        // $('#button').text($(".btn").val());
-//});
- 
-//});
