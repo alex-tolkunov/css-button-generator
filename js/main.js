b4d3c0b9317c97f3f38432b5514a632ffa0d553f
unknown <tas23alex@mail.ru>
2014-09-06 02:43:32 +0400

    Реализовано изменение ширины и высоты кнопки

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@
             this.updateResult(); 
             this.setUpPlugins();
             this.setUpPluginsTwo();
+            this.setUpPluginsThree();
+            this.setUpPluginsFour();
             this.htmlArea();
+
         },
         setUpListeners: function () {
           //Slider(Ползунки)
@@ -19,11 +22,19 @@
           $ ('#btn-text').on('keyup',$.proxy(this.BtnText, this));
           $ ('#text-color').on('change',$.proxy(this.BtnColorText, this));
           $ ('#font').on('change',$.proxy(this.BtnFontText, this)); 
+
+          $ ('#posx').on('change',$.proxy(this.PosX, this));
+          $ ('#posy').on('change',$.proxy(this.PosY, this));
+          $ ('#shadow-color').on('change',$.proxy(this.shadowColor, this));
+
+          $ ('#slider-width').on('slidechange',$.proxy(this.SliderWidth, this));
+          $ ('#slider-height').on('slidechange',$.proxy(this.SliderHeiht, this));
         },
 
         //Кнопка
         create : $ ('.result-button'),
-
+        
+        
         //Slider UI border-radius
         setUpPlugins:function() {
             $( "#slider-border-radius" ).slider(
@@ -56,6 +67,44 @@
         borderSizeChange:function (e,ui) {
             this.create.css({
                 'border-width' : ui.value
+            });
+            this.updateResult();
+        },
+
+        //Slider UI Height        
+        setUpPluginsThree:function() {
+            $( "#slider-height" ).slider(
+              {
+              range: 'min',
+              max: 30,
+              min: 0,
+              });
+        },
+
+        //Изменение высоты
+        SliderHeiht:function (e,ui) {
+            this.create.css({
+                'padding-top' : ui.value,
+                'padding-bottom' : ui.value,
+            });
+            this.updateResult();
+        },
+
+        //Slider UI Width
+        setUpPluginsFour:function() {
+            $( "#slider-width" ).slider(
+              {
+              range: 'min',
+              max: 60,
+              min: 0,
+            });
+        },
+
+        //Изменение ширины
+        SliderWidth:function (e,ui) {
+            this.create.css({
+                'padding-left' : ui.value,
+                'padding-right' : ui.value,
             });
             this.updateResult();
         },
@@ -110,18 +159,22 @@
                 bgcolor = this.create.css('background-color'),
                 brcolor = this.create.css('border-color'),
                 textcolor = this.create.css('color'),
+                Pad = this.create.css('padding-top') + ' ' + this.create.css('padding-left'),
+                
+                
                 cssCodeResultArea = $('#resultcss');
 
           cssCodeResultArea.text(
             '.button {\n' +
             'font-family:'           + fontFamily + ';\n' +
+            'padding:'               + Pad + ';\n' +
             'background-color:'      + bgcolor + ';\n' +
-            'border-color:'          + brcolor + ';\n' +
+            'border:'                + borderSize +  ' solid '  + brcolor + ';\n' +
             'color:'                 + textcolor + ';\n' +
             '-webkit-border-radius:' + borderRad + ';\n' +
-            '-moz-border-radius:'    + borderRad + ';\n' +
-            'border-radius:'         + borderRad + ';\n' +
-            'border-width:'          + borderSize + ';\n' +
+            '   -moz-border-radius:'    + borderRad + ';\n' +
+            '             border-radius:'         + borderRad + ';\n' +
+            
             '};'
           );
 
